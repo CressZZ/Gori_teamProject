bdeaa14e15300e0e3d73a06fc0e975eeb7ab5f14
YoungJin Park <youngjinpark@YoungJinui-MacBook-Pro.local>
2017-04-05 00:25:01 +0900

    lec 페이지의 모듈화 작업 진행 도중 입니다.

---
+++ 02.Code/gori/src/components/Lec_class-nav.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <!-- class_nav -->
+  <section class="class_nav row">
+    <nav class="class_nav__wrapper col-4-4 col-7-12">
+      <ul>
+        <li class="right-border"><a href="#" >수업소개</a></li>
+        <li><a href="#" class="">리뷰보기</a></li>
+        <li class="left-border"><a href="#" >문의하기</a></li>
+      </ul>
+    </nav>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+  <style lang="sass">
+    @import "../sass/gen_source"
+    @import "../sass/gen_mixin"
+    @import "../sass/lec_class-nav"
+  </style>

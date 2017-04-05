b06e6d66cb34c0900ab9ec8026792fd837376e42
jiwoo-beak <hwawitch@gmail.com>
2017-04-05 17:51:23 +0900

    모달 로그인 / 회원가입 모듈화 완성

--- 02.Code/gori/src/routes.js
+++ 02.Code/gori/src/routes.js
@@ -2,6 +2,8 @@ import Main from './components/Main.vue'
 import Lec from './components/Lec.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Join from './components/Join.vue'
+import Login from './components/Login.vue'
 
 export const routes = [
   {
@@ -10,7 +12,10 @@ export const routes = [
     components: {
       'default': Main,
       'header': Header,
-      'footer': Footer
+      'footer': Footer,
+      'join': Join,
+      'login': Login,
+
     }
   },
   {
@@ -19,7 +24,9 @@ export const routes = [
     components: {
       'default': Lec,
       'header': Header,
-      'footer': Footer
+      'footer': Footer,
+      'join': Join,
+      'login': Login,
       // Map: Map
     }
   },

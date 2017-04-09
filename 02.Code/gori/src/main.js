2ee06236f3c22019ebc4c1c06dfc14095d6b5efa
YoungJin Park <youngjinpark@YoungJinui-MacBook-Pro.local>
2017-04-09 10:51:45 +0900

    vue-resource 부분 추가

--- 02.Code/gori/src/main.js
+++ 02.Code/gori/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import {routes} from './routes'
 import App from './App.vue'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://gori-f0de6.firebaseio.com/';
+// Vue.http.headers.common['Authorization'] = 'Token '
 
 const router = new VueRouter({
   mode: 'history',

354c3a8cf0df27373ae596cd1fefba9ebed73559
YoungJin Park <youngjinpark@YoungJinui-MacBook-Pro.local>
2017-04-10 09:52:43 +0900

    기존에는 한 페이지의 하단을 보고 있는 상태에서 페이지를 이동 하였을때, 똑같이 페이지 하단을 보여주는 문제가 발생함. 하지만, 해당 부분을 위한 메서드가 존재하여 활용 하였음 (scrollBehavior (to, from, savedPosition)

--- 02.Code/gori/src/main.js
+++ 02.Code/gori/src/main.js
@@ -4,15 +4,18 @@ import VueResource from 'vue-resource'
 import {routes} from './routes'
 import App from './App.vue'
 
+// require('./fontello/css/fontello.css');
+
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-Vue.http.options.root = 'https://gori-f0de6.firebaseio.com/';
-// Vue.http.headers.common['Authorization'] = 'Token '
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+  return { x: 0, y: 0 }
+},
 });
 
 new Vue({

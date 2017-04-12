90c60958cb311a3645df31492bebd5b5549721c4
jiwoo-beak <hwawitch@gmail.com>
2017-04-12 17:10:46 +0900

    conflicttttt

--- 02.Code/gori/src/components/Lec_speaking.vue
+++ 02.Code/gori/src/components/Lec_speaking.vue
@@ -2,10 +2,9 @@
   <!-- speaking -->
   <section class="speaking">
     <h2 class="hidden__heading">튜터소개</h2>
-    <div class="row">
+    <div class="speaking__wrapper">
 
       <div class="row">
-
         <p class="speaking__descrip col-4-4 col-7-12"></p>
         <img :src="`${detailAll.tutor.profile_image}`" :alt="`${detailAll.tutor.nickname}`" class="speaking__tutor-picture  col-1-4">
         <strong class="speaking__tutor-speaking  col-5-12  push-2-12"> "{{detailAll.tutor_info ? detailAll.tutor_info : ""}}"</strong>
@@ -13,7 +12,7 @@
 
       </div>
       <!-- <p class="speaking__descrip col-4-4 col-7-12">Lorem ipsum dolor sit amet.Lorem ipsum dolor sit ametLorem ipsum dolor sit amet</p> -->
-
+  
   </section>
 </template>
 

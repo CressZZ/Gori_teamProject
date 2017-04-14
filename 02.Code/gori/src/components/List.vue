6a0662152e5e7d41d3c3bea3c035c78f9ac37774
YoungJin Park <youngjinpark@YoungJinui-MacBook-Pro.local>
2017-04-14 10:34:47 +0900

    list페이지 추가

---
+++ 02.Code/gori/src/components/List.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+    <div>
+      <list-search></list-search>
+      <list-class-list :classlist ="classlist"></list-class-list>
+    </div>
+</template>
+
+<script>
+
+import ListSearch from './List_search.vue'
+import ListClassList from './List_class_list.vue'
+
+
+
+
+export default {
+  // props: ['classlist'],
+
+
+  data(){
+    return {
+      // classlist:[],
+    }
+  },
+  props: ['classlist'],
+  components: {
+    ListSearch,
+    ListClassList,
+
+  },
+  created(){
+    // this.$http.get('https://mozzi.co.kr/api/talent/list/')
+    // .then(function(response){
+    //   return response.json()
+    // })
+    // .then(function(data){
+    //   this.classlist = data
+    // })
+    // .then(function(){
+    // })
+
+  },
+
+}
+</script>
+
+<style lang="sass">
+  // @import "../sass/total"
+  // @import "../sass/main-index"
+</style>
